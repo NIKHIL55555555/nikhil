93948a42c083f18e0d46bafa7b868d30b3232e79
NIKHIL55555555 <103985173+NIKHIL55555555@users.noreply.github.com>
2022-12-09 16:10:55 +0530

    Update task53D.py

--- task53D.py
+++ task53D.py
@@ -6,33 +6,33 @@ from tkinter import ttk
 import time
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
-win = Tk()
-win.title("MORSE CODE BLINKING")
+win = Tk()      // open gui window
+win.title("MORSE CODE BLINKING")  // set window title
 
-redled = LED(18)
+redled = LED(18)   // set the variable of the red led
 
-variable= Entry(win, width = 40, bg = "blue", fg = "white")
+variable= Entry(win, width = 40, bg = "blue", fg = "white") // customizing the opened window set background color, width
 variable.focus_set()
 variable.pack()
 
-def dash():
-    redled.on()
-    time.sleep(2.0)
-    redled.off()
-    time.sleep(0.25)
+def dash():              // creating a function
+    redled.on()          //turn the led on 
+    time.sleep(2.0)      // delay of 200 milli second
+    redled.off()         //turn the led off 
+    time.sleep(0.25)     // delay of 25 milli second
 
 def dot():
-    redled.on()
-    time.sleep(0.75)
-    redled.off()
-    time.sleep(0.25)
+    redled.on()          //turn the led on 
+    time.sleep(0.75)     // delay of 75 milli second
+    redled.off()         //turn the led off
+    time.sleep(0.25)     // delay of 25 milli second
 
-def nameconverter():
-    name = variable.get()
-    text = name.upper()
-    if len(name) < 13:
+def nameconverter():         // created function
+    name = variable.get()    // getting the user input
+    text = name.upper()      // set input in capital letter
+    if len(name) < 13:       // set the length of the input to 13
         i=0
-        while i < (len(name)):
+        while i < (len(name)):      // initilizing the functions according to the morse code     
             if text[i] == 'A':
                 dot()
                 dash()
@@ -145,18 +145,18 @@ def nameconverter():
                 time.sleep(2)
             i = i + 1
     else:
-        print("ENTER YOUR TEXT LESS THAN 12 WORDS")
+        print("ENTER YOUR TEXT LESS THAN 12 WORDS")      // if the input is gretear than 13 
 
 
-def close():
-    RPi.GPIO.cleanup()
+def close():       // created function
+    RPi.GPIO.cleanup()     // close the poped window
     win.destroy()
 
 
-submitbutton = Button(win, text = "SUBMIT", command = nameconverter, bg = "yellow", fg = "black")
+submitbutton = Button(win, text = "SUBMIT", command = nameconverter, bg = "yellow", fg = "black")   // created a submit button in gui 
 submitbutton.pack()
 
-exitbutton = Button(win, text = "EXIT", command = close, bg = "Red", fg = "black")
+exitbutton = Button(win, text = "EXIT", command = close, bg = "Red", fg = "black")  // for exiting from the window
 exitbutton.pack()
 
 win.mainloop()
